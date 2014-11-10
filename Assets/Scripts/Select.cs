bfad6113f51cb7f97e7ff495d5d6dde77f1157f7
Tim Skew <timskew@gmail.com>
2014-11-10 00:54:03 +0000

    Adds an enemy placeholder model and simple targetting in aim mode

---
+++ Assets/Scripts/Select.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Select : MonoBehaviour {
+	public RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			OnMouseDown();
+		}
+	}
+
+	void OnMouseDown () {
+		Ray ray = Camera.main.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2, 0));
+		Debug.Log ("Fired ray");
+
+		if (Physics.Raycast(ray, out hit, 100)){
+			Debug.Log("Hit object");
+			hit.collider.gameObject.GetComponent<Targeted>().OnTarget();
+		}	}
+}
---
+++ Assets/Scripts/Targeted.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targeted : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTarget() {
+		Debug.Log ("Targetted");
+		renderer.material.SetColor ("_OutlineColor", Color.red);
+	}
+}
